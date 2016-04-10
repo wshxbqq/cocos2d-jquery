4ad4e36e3223190dd884cdf99abfebfdf11536e8
shaoxing.wang <wshxbqq@gmail.com>
2016-04-11 03:57:51 +0800

    增加 jsb支持

--- src/Util/JQ/JQCore.js
+++ src/Util/JQ/JQCore.js
@@ -1,48 +1,71 @@
+ var jqCore = function(root, selector) {
+     var _this = this;
+     this.items = [];
 
-var jqCore = function (root, selector) {
-    var _this = this;
-    this.items = [];
-    var tps = typeof(root) + "_" + typeof(selector);
-    switch (tps) {
-        case "string_undefined":
-            this.selector = root;
-            this.root = cc.director.getRunningScene();
-            this.items = CocosSelector(this.root, this.selector);
-            ; break;
+     var tps = Object.prototype.toString.call(root) + "_" + Object.prototype.toString.call(selector);
 
-        case "object_undefined":
-            var objInfo=Object.prototype.toString.call(root)
-            if(objInfo=="[object Array]"){
-                this.items = root;
-            }else{
-                this.items = [root];
-            }
-            ; break;
+     if (cc.sys.isNative) {
+         switch (tps) {
+             case "[object String]_[object Undefined]":
+                 this.selector = root;
+                 this.root = cc.director.getRunningScene();
+                 this.items = CocosSelector(this.root, this.selector);;
+                 break;
+             case "[object Node]_[object Undefined]":
+                 this.items = [root];;
+                 break;
+             case "[object Array]_[object Undefined]":
+                 this.items = root;;
+                 break;
+             case "[object Node]_[object String]":
+                 this.selector = selector;
+                 this.root = root;
+                 this.items = CocosSelector(this.root, this.selector);;
+                 break;
+             case "[object Undefined]_[object Undefined]":
+                 ;
+                 break;
+             default:
+                 return;
+                 break;
 
-        case "object_string":
-            this.selector = selector;
-            this.root = root;
-            this.items = CocosSelector(this.root, this.selector);
-            ; break;
+         }
 
-        case "undefined_undefined":;break;
-        default: return; break;
+     } else {
+         switch (tps) {
+             case "[object String]_[object Undefined]":
+                 this.selector = root;
+                 this.root = cc.director.getRunningScene();
+                 this.items = CocosSelector(this.root, this.selector);;
+                 break;
+             case "[object Object]_[object Undefined]":
+                 this.items = [root];;
+                 break;
+             case "[object Array]_[object Undefined]":
+                 this.items = root;;
+                 break;
+             case "[object Object]_[object String]":
+                 this.selector = selector;
+                 this.root = root;
+                 this.items = CocosSelector(this.root, this.selector);;
+                 break;
+             case "[object Undefined]_[object Undefined]":
+                 ;
+                 break;
+             default:
+                 return;
+                 break;
 
-    }
+         }
 
-    this.length = this.items.length;
-    for (var i = 0; i < this.items.length; i++) {
-        this[i] = this.items[i];
-    }
+     }
 
 
-  
-};
-
+     this.length = this.items.length;
+     for (var i = 0; i < this.items.length; i++) {
+         this[i] = this.items[i];
+     }
 
 
 
-
-
- 
- 
+ };
